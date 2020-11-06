f1acab3a362b958f815e9b730783a2873a6edac3
André Mathisen <9752808+anzure@users.noreply.github.com>
2020-11-06 15:23:02 +0100

    Initial commit

---
+++ src/main/java/no/ehfsok/dao/CompanyRepo.java
@@ -0,0 +1,8 @@
+package no.ehfsok.dao;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CompanyRepo {
+
+}
---
+++ src/main/java/no/ehfsok/model/Company.java
@@ -0,0 +1,8 @@
+package no.ehfsok.model;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Company {
+
+}
