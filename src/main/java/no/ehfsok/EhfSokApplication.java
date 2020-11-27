3b950170998063e16e2f775407683188c0463c6f
André Mathisen <9752808+anzure@users.noreply.github.com>
2020-11-27 12:25:02 +0100

    🔧 Redigert Spring-innstillinger.

--- src/main/java/no/ehfsok/EhfSokApplication.java
+++ src/main/java/no/ehfsok/EhfSokApplication.java
@@ -2,10 +2,11 @@ package no.ehfsok;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 @EnableJpaRepositories
 @ServletComponentScan
 public class EhfSokApplication {
