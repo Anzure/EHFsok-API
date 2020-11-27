9bbc13ef6929e35f63274775dd65d8bacaa6f34e
André Mathisen <9752808+anzure@users.noreply.github.com>
2020-11-27 12:26:12 +0100

    🐛 Fikset database bugg.

--- src/main/java/no/ehfsok/EhfSokApplication.java
+++ src/main/java/no/ehfsok/EhfSokApplication.java
@@ -2,11 +2,10 @@ package no.ehfsok;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@SpringBootApplication
 @EnableJpaRepositories
 @ServletComponentScan
 public class EhfSokApplication {
